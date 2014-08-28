cd7d97c10fb244f414c85c0259ceb53be1bcf76c
GrimDemon <wispwind@gmail.com>
2014-08-28 23:53:17 +0200

    Changes for Polcoin

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xa6, 0x73, 0x60, 0x83 };
 
 CMessageHeader::CMessageHeader()
 {
